6293a14c804954cd84c26d84d94d18aab2dfc2f1
hirata erika <biiiiii.uw@gmail.com>
2020-12-08 14:28:48 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$function {
-    
-}
+// $(function() {
+//     // articleをホバーで発動
+//     $('p::after').hover(
+//         function() {
+//         //     $('p::after').css('width','50px');
+//         // },
+//         // function() {
+//         //     $('p::after').css('width',)
+//         // }
+//         $(this).toggleClass('.ling_lg');
+//         });
+// });
